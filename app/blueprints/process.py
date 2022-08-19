fa6d03cad7fa29f09f6d22113d51b715b601a332
Vince Maina <vchapandrews@gmail.com>
2022-08-19 19:35:12 +0100

    Incorporated image_generation function into flask app

--- app/blueprints/process.py
+++ app/blueprints/process.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, send_file
 
 bp = Blueprint('process', __name__, url_prefix='/process')
 
@@ -7,4 +7,23 @@ def single_file():
 
     args = request.args
 
-    return args
+    root_directory = 'user_files'
+    file_directory = args['upload_folder']
+    file_name = args['file_name']
+    output_format = args['output_format']
+
+    import os
+    file_path = os.path.join(root_directory, file_directory, file_name)
+
+    print('FILE PATH:', file_path)
+
+    from image_generator import generate
+
+    output_path = generate(file_path, output_format)
+    print('OUTPUT', output_path)
+
+    output_path = output_path.replace('app/', '')
+
+    print('OUTPUT', output_path)
+
+    return send_file(output_path)
