35d875c3d89f3b53aafa035d727ce1e2cf6057ce
Vince Maina <vchapandrews@gmail.com>
2022-08-19 13:03:22 +0100

    Adding placeholder process functions Displays the information that the user has uploaded

--- app/blueprints/process.py
+++ app/blueprints/process.py
@@ -1,3 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-bp = Blueprint('process', __name__, '/process')
+bp = Blueprint('process', __name__, url_prefix='/process')
+
+@bp.route('', methods=['GET', 'POST'])
+def single_file():
+
+    args = request.args
+
+    return args
