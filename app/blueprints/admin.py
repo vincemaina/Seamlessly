d4371c54e051efa98966b8615cfbe4591032b902
Vince Maina <vchapandrews@gmail.com>
2022-08-19 19:34:56 +0100

    Added functionality to view generated files

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template, current_app, request, redirect, url_for
+from flask import Blueprint, render_template, current_app, request, redirect, url_for, send_from_directory, send_file
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-@bp.route('/purge_cache', methods=['POST'])
-def purge_cache():
+@bp.route('/purge_user_files', methods=['POST'])
+def purge_user_files():
     from pathlib import Path
     import shutil
 
@@ -23,7 +23,7 @@ def view_user_files():
 
             # The user clicked the purge button (or the purge request was trigged by a POST request.)
 
-            purge_cache()
+            purge_user_files()
 
     import os
     from pathlib import Path
@@ -46,5 +46,63 @@ def view_user_files():
 
     return render_template('admin/view_user_files.html', user_files=user_files_directory, sorted_directory=sorted_directory)
 
-if __name__ == '__main__':
-    view_user_files()
+
+@bp.route('/purge_generated_media', methods=['POST'])
+def purge_generated_media():
+    from pathlib import Path
+    import shutil
+
+    dirpath = Path('app/generated_media')
+    if dirpath.exists() and dirpath.is_dir():
+        shutil.rmtree(dirpath)
+        print('Removed directory.')
+    
+    return redirect(url_for('admin.view_user_files'))
+
+@bp.route('/view_generated_media', methods=['GET', 'POST'])
+def view_generated_media():
+
+    root = 'generated_media'
+
+    if request.method == 'POST':
+
+        try:
+            
+            request.form['purge-button']
+            purge_generated_media()
+
+        except KeyError:
+
+            from pathlib import Path
+
+            file_path = Path(root, request.form['file_to_download'])
+
+            # root, subdirectory, file_name = file_path.parts
+
+            # print('PATH:', file_path)
+
+            # print('FILE NAME:', file_name)
+
+            return send_file(file_path)
+
+    import os
+    from pathlib import Path
+
+    generated_media_directory = {}
+
+    root = 'app/generated_media'
+
+    with current_app.app_context():
+        for path, subdirs, files in os.walk(root):
+            for name in files:
+                sub_directory, file = Path(path, name).parts[-2:]
+                try:
+                    generated_media_directory[sub_directory]
+                except:
+                    generated_media_directory[sub_directory] = []
+                finally:
+                    generated_media_directory[sub_directory].append(file)
+
+    sorted_directory = sorted(generated_media_directory, reverse=True)
+
+    return render_template('admin/view_generated_media.html', user_files=generated_media_directory, sorted_directory=sorted_directory)
