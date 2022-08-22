cb3dc3d49633d22a29621737047ba17f5e5c4949
Vince Maina <vchapandrews@gmail.com>
2022-08-22 20:29:34 +0100

    Rearranging doc

--- app/config.py
+++ app/config.py
@@ -1,14 +1,19 @@
-# Accepted file types for the files this program generates
-output_file_types = {
-    'Image (e.g. Jpeg, Png)': 'webp',
-    'Animation (e.g. Gif)': 'webp',
+# File limit in MBs
+file_limit_megabytes = 25
+
+# Accepted input file formats, here is a list of the filetypes we can identify: https://pypi.org/project/filetype/
+INPUT_FILE_TYPES = {
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif',
 }
 
-# File limit in MBs
-file_limit_megabytes = 3
-
-# Accepted input file formats
-INPUT_FILE_TYPES = {'png', 'jpg', 'jpeg', 'gif', 'webp', 'mp4', 'mov', 'mpeg'}
+# Accepted file types for the files this program generates
+OUTPUT_FILE_TYPES = {
+    'Image': 'webp',
+    'Animation': 'webp',
+}
 
 # Path to file upload location
 UPLOAD_FOLDER = 'user_files'
