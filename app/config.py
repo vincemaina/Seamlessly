8338d090fd0c465b12a90aef5c51eedf8c8a33cd
Vince Maina <vchapandrews@gmail.com>
2022-08-22 11:53:47 +0100

    Added a single source for setting configuration variables This is useful where the same variables need to be used multiple times in a program, we can update and access them from a single file, which also reduces the risk of having contradicting values

---
+++ app/config.py
@@ -0,0 +1,8 @@
+# Accepted file types for the files this program generates
+output_file_types = {
+    'Image (e.g. Jpeg, Png)': 'webp',
+    'Animation (e.g. Gif)': 'webp',
+}
+
+# File limit in MBs
+file_limit_megabytes = 3
