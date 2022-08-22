31540c340e6ae948d8e389d2b75b3bdd4f9047f3
Vince Maina <vchapandrews@gmail.com>
2022-08-22 15:09:05 +0100

    Whitespace changes

--- app/blueprints/configure_css_background.py
+++ app/blueprints/configure_css_background.py
@@ -12,7 +12,6 @@ def configure():
     print('FILE PATH:', file_path)
 
     from aws_s3.create_presigned_url import create_presigned_url
-
     image_url = create_presigned_url(file_path)
 
     return render_template('file_upload/optimise_for_web_background.html', file_path=file_path, image_url=image_url)
