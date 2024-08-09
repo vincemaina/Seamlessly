1cf8d8ca61b7001f1e72c996f7a45c2c534761d6
Vince Maina <vincemaina@proton.me>
2024-08-09 11:37:45 +0100

    fix: remove ref to aws package

--- app/blueprints/configure_css_background.py
+++ app/blueprints/configure_css_background.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, send_file, send_from_directory
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('configure_css', __name__, url_prefix='/configure')
 
@@ -11,7 +11,4 @@ def configure():
 
     print('FILE PATH:', file_path)
 
-    from aws_s3.create_presigned_url import create_presigned_url
-    image_url = create_presigned_url(file_path)
-
-    return render_template('file_upload/optimise_for_web_background.html', file_path=file_path, image_url=image_url)
+    return render_template('file_upload/optimise_for_web_background.html', file_path=file_path, image_url=f'/static/{file_path}')
